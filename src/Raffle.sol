673a6049350876fb690dce8d2afdb83ac01a0670
Vention <backstreetal6year@gmail.com>
2024-05-17 10:18:29 +0800

    request random number using chainlink, import chainlink library

--- src/Raffle.sol
+++ src/Raffle.sol
@@ -1,38 +1,13 @@
 // SPDX-License-Identifier: SEE LICENSE IN LICENSE
 pragma solidity ^0.8.18;
+import {VRFCoordinatorV2Interface} from "@chainlink/contracts/v0.8/vrf/interfaces/VRFCoordinatorV2Interface.sol";
+import {VRFConsumerBaseV2} from "@chainlink/contracts/v0.8/vrf/VRFConsumerBaseV2.sol";
+
 error theEnterFeeisNotEnough();
+error theWinnerWithdrawFailed();
 
 
-contract Raffle {
-    // //uint256 private s_ticketPrice;
-    // uint256 private immutable i_ticketPrice;
-    // //external is more gas effixiotn than public
-    // uint256 private immutable i_interval;
-    // address private immutable i_vrfCoordinator;
-
-
-    // constructor(uint256 ticketPrice, uint256 interval, address vrfCoordinator) {  
-    //     //s_ticketPrice = ticketPrice; 
-    //     i_ticketPrice = ticketPrice;
-    //     i_interval = interval;
-    //     i_vrfCoordinator = vrfCoordinator;
-    // }
-    // function buyTheTicket() external payable {
-        
-    // }
-
-    // function pickWinner()  public returns (uint256) {
-        
-    // }
-
-    // Every method have to has visibility
-    // function getTicketPrice() public view returns (uint256){
-    //     return i_ticketPrice;
-    // }
-
-    //custom error for not meet the value of the ticket
-    //Better name the error start with the contract
-    //todo 
+contract Raffle is VRFConsumerBaseV2{
 
     // payable is meant for receive but not send Ether
     uint256 private immutable i_enterRaffleFee;
@@ -41,6 +16,29 @@ contract Raffle {
     uint256 private immutable i_interval;
     uint256 private s_timestamp;
 
+   /**
+    * the random words required
+    * 1. the address of the vrfcoordinator (based on diff network)
+    * 2. the gas lane (base on diff network get from chainlink)
+    * 3. the subscribtion id
+    * +++ the number of request confirmation
+    * 4. call back gas limit
+    * 5. how many random number you want to have in one request
+    */
+    //bytes32 private immutable i_vrfCoordinator;
+    //address private immutable i_vrfCoordinator;
+    bytes32 private immutable i_gasLane;
+    //bytes32 private immutable i_subscribtionId;
+    uint64 private immutable i_subscribtionId;
+    //Error (9553): Invalid type for argument in function call. Invalid implicit conversion from uint256 to uint16 requested.
+    uint16 private constant REQUEST_CONFIRMATION = 3;
+    uint32 private immutable i_gasLimit;
+    //uint256 private immutable i_numWords;
+    uint32 private constant NUMWORDS = 1;
+    VRFCoordinatorV2Interface private immutable i_vrfCoordinator;
+
+    address private s_recentWinner;
+
     //?How to properly state the Error
     //it should be lower case... error
     //Error (theEnterFeeisNotEnough);
@@ -48,10 +46,26 @@ contract Raffle {
     //uscase of the event remain unknown
     event EnteredUser(address indexed user);
 
-    constructor(uint256 enterRaffleFee, uint256 interval){
+    /**
+     * HARDCODED FOR SEPOLIA
+     * COORDINATOR: 0x8103B0A8A00be2DDC778e6e7eaa21791Cd364625
+     */
+    constructor(uint256 enterRaffleFee, 
+                uint256 interval, 
+                bytes32 gasLane,
+                address vrfCoordinator,
+                uint64 subscribtionId,
+                uint32 gasLimit) VRFConsumerBaseV2(vrfCoordinator){
         i_enterRaffleFee = enterRaffleFee;
         i_interval = interval;
         s_timestamp = block.timestamp;
+
+        i_vrfCoordinator = VRFCoordinatorV2Interface(vrfCoordinator);
+        //i_vrfCoordinator = VRFCoordinatorV2Interface(0x8103B0A8A00be2DDC778e6e7eaa21791Cd364625);
+        i_gasLane = gasLane;
+        i_subscribtionId = subscribtionId;
+        i_gasLimit = gasLimit;
+
     }
 
     function enterRaffle() external payable {
@@ -70,23 +84,36 @@ contract Raffle {
     //1. Get a random number using chainlink VRF
     //2. Used the random number to pick a player
     //3. Be automatically called
-   /**
-    * the random words required
-    * 1. the address of the vrfcoordinator (based on diff network)
-    * 2. the gas lane (base on diff network get from chainlink)
-    * 3. the subscribtion id
-    * 4. call back gas limit
-    */
+
     function pickWinner() external {
         if((block.timestamp - s_timestamp)> i_interval){
             s_timestamp = block.timestamp;
         }else{
             revert();
         }
+        // This is just done the request, now we have to get the nubmer back
+        // So this is the step one, now we have to have the step 2 to get the number back
+        uint256 requestId = i_vrfCoordinator.requestRandomWords(
+            i_gasLane, 
+            i_subscribtionId, 
+            REQUEST_CONFIRMATION, 
+            i_gasLimit, 
+            NUMWORDS
+        );
+    }
+    //the request ID in the parameter should be the same we have in the above fucntion
+    function fulfillRandomWords(uint256 requestId, uint256[] memory randomWords) internal override{
+        uint256 indexOfWinner = randomWords[0] % s_userAddress.length;
+        address payable winner = s_userAddress[indexOfWinner];
+        s_recentWinner = winner;
 
-        uint256 requestId =  i_vrfCoordinator.requestRandomWords(
-            
-        )
+        //.call -> more control over the error handling 
+        //.transfer -> encounter en error dufing the transfer, it reverts the transaction entirely
+        // to be continue
+        (bool success, ) = winner.call{value: address(this).balance}("");
+        if(!success) {
+            revert theWinnerWithdrawFailed();
+        }
     }
 
     function getEnterRaffleFee() external view returns(uint256){
