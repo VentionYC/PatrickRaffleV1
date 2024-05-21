3629fd87a9507ec24fa060bff47a313fc185ca0f
Vention <backstreetal6year@gmail.com>
2024-05-21 17:06:54 +0800

    roughly finished raffle project

--- README.md
+++ README.md
@@ -3,3 +3,27 @@ User to use(pay fee) --> (After " " time)fee for (randomly picked)winner to draw
 # Useful resouces
  * Chain link VRF -> Get a randomness number
  * Chainlink Automation -> Time based trigger for excute a method
+
+ # Payable modifier
+ * It's meant for receive but not send Ether, address.. function...
+
+ # Layout of Contract:
+// version
+// imports
+// errors
+// interfaces, libraries, contracts
+// Type declarations
+// State variables
+// Events
+// Modifiers
+// Functions
+
+# Layout of Functions:
+// constructor
+// receive function (if exists)
+// fallback function (if exists)
+// external
+// public
+// internal
+// private
+// view & pure functions
--- src/Raffle.sol
+++ src/Raffle.sol
@@ -1,20 +1,33 @@
 // SPDX-License-Identifier: SEE LICENSE IN LICENSE
-pragma solidity ^0.8.18;
+pragma solidity ^0.8.0;
 import {VRFCoordinatorV2Interface} from "@chainlink/contracts/v0.8/vrf/interfaces/VRFCoordinatorV2Interface.sol";
 import {VRFConsumerBaseV2} from "@chainlink/contracts/v0.8/vrf/VRFConsumerBaseV2.sol";
+import {AutomationCompatibleInterface} from "@chainlink/contracts/v0.8/interfaces/AutomationCompatibleInterface.sol";
 
 error theEnterFeeisNotEnough();
 error theWinnerWithdrawFailed();
+error Raffle_UpkeepNotNeeded(
+    uint256 contractBalance,
+    uint256 userCount
+);
+error Raffle_RaffleNotOpen();
 
 
-contract Raffle is VRFConsumerBaseV2{
+contract Raffle is VRFConsumerBaseV2, AutomationCompatibleInterface{
 
-    // payable is meant for receive but not send Ether
+    enum RaffleState {
+        OPEN,
+        CALCULATING
+    }
+
+    VRFCoordinatorV2Interface private immutable i_vrfCoordinator;
+
+    //Basic enter fee, user address(last winner record), between time and start time
     uint256 private immutable i_enterRaffleFee;
     address payable[] private s_userAddress;
-    //?I don't understand why we are using this since we have the Chainlink can auto call a method with interval
     uint256 private immutable i_interval;
     uint256 private s_timestamp;
+    address private s_recentWinner;
 
    /**
     * the random words required
@@ -25,26 +38,16 @@ contract Raffle is VRFConsumerBaseV2{
     * 4. call back gas limit
     * 5. how many random number you want to have in one request
     */
-    //bytes32 private immutable i_vrfCoordinator;
-    //address private immutable i_vrfCoordinator;
     bytes32 private immutable i_gasLane;
-    //bytes32 private immutable i_subscribtionId;
     uint64 private immutable i_subscribtionId;
-    //Error (9553): Invalid type for argument in function call. Invalid implicit conversion from uint256 to uint16 requested.
     uint16 private constant REQUEST_CONFIRMATION = 3;
     uint32 private immutable i_gasLimit;
-    //uint256 private immutable i_numWords;
     uint32 private constant NUMWORDS = 1;
-    VRFCoordinatorV2Interface private immutable i_vrfCoordinator;
 
-    address private s_recentWinner;
+    RaffleState private s_raffleState;
 
-    //?How to properly state the Error
-    //it should be lower case... error
-    //Error (theEnterFeeisNotEnough);
-
-    //uscase of the event remain unknown
     event EnteredUser(address indexed user);
+    event RequestRaffleWinner(uint256 indexed winner);
 
     /**
      * HARDCODED FOR SEPOLIA
@@ -65,7 +68,9 @@ contract Raffle is VRFConsumerBaseV2{
         i_gasLane = gasLane;
         i_subscribtionId = subscribtionId;
         i_gasLimit = gasLimit;
+        
 
+        s_raffleState = RaffleState.OPEN;
     }
 
     function enterRaffle() external payable {
@@ -74,7 +79,9 @@ contract Raffle is VRFConsumerBaseV2{
         //require(msg.value >= i_enterRaffleFee, "the enter fee is not enough");
         if(msg.value < i_enterRaffleFee){
             revert theEnterFeeisNotEnough();
-        }else {
+        }else if (s_raffleState != RaffleState.OPEN) {
+            revert Raffle_RaffleNotOpen();
+        }else{
             //s_userAddress.push(msg.sender);
             s_userAddress.push(payable(msg.sender));
             emit EnteredUser(msg.sender);
@@ -85,15 +92,16 @@ contract Raffle is VRFConsumerBaseV2{
     //2. Used the random number to pick a player
     //3. Be automatically called
 
-    function pickWinner() external {
-        if((block.timestamp - s_timestamp)> i_interval){
-            s_timestamp = block.timestamp;
-        }else{
-            revert();
-        }
+    function pickWinner() internal returns (uint256 requestId) {
+        //into the performupkeep function
+        // if((block.timestamp - s_timestamp)> i_interval){
+        //     s_timestamp = block.timestamp;
+        // }else{
+        //     revert();
+        // }
         // This is just done the request, now we have to get the nubmer back
         // So this is the step one, now we have to have the step 2 to get the number back
-        uint256 requestId = i_vrfCoordinator.requestRandomWords(
+        requestId = i_vrfCoordinator.requestRandomWords(
             i_gasLane, 
             i_subscribtionId, 
             REQUEST_CONFIRMATION, 
@@ -102,7 +110,7 @@ contract Raffle is VRFConsumerBaseV2{
         );
     }
     //the request ID in the parameter should be the same we have in the above fucntion
-    function fulfillRandomWords(uint256 requestId, uint256[] memory randomWords) internal override{
+    function fulfillRandomWords(uint256 /*requestId*/, uint256[] memory randomWords) internal override{
         uint256 indexOfWinner = randomWords[0] % s_userAddress.length;
         address payable winner = s_userAddress[indexOfWinner];
         s_recentWinner = winner;
@@ -113,6 +121,8 @@ contract Raffle is VRFConsumerBaseV2{
         (bool success, ) = winner.call{value: address(this).balance}("");
         if(!success) {
             revert theWinnerWithdrawFailed();
+        }else{
+            s_raffleState = RaffleState.OPEN;
         }
     }
 
@@ -126,4 +136,36 @@ contract Raffle is VRFConsumerBaseV2{
     //that we should override
     //let's back to the question that why we still have to count the time whjen we have the chainlink to set the timer for us, because except the time factor, we still have
     // other factor like the balace or other things we should contcer about off the chain, so it's ok to have a seperate limit.
+
+    function checkUpkeep(
+        bytes memory /*checkData*/
+        ) public view override returns (
+            bool upkeepNeeded, 
+            bytes memory /*performData*/){
+                bool isOpen = RaffleState.OPEN == s_raffleState;
+
+                bool timePassed = ((block.timestamp - s_timestamp) > i_interval);
+                bool hasPlayers = s_userAddress.length > 0;
+                bool hasBalance = address(this).balance >0;
+                upkeepNeeded = (timePassed && hasBalance && hasPlayers && isOpen);
+                return (upkeepNeeded, "0x0");
+
+    }
+
+    function performUpkeep(bytes memory /*performData*/) external  override{
+        (bool upkeepNeeded, )  =  checkUpkeep("");
+        if (!upkeepNeeded) {
+            //degub purpose
+            revert Raffle_UpkeepNotNeeded(
+                address(this).balance,
+                s_userAddress.length
+            );
+        }
+
+        s_raffleState = RaffleState.CALCULATING;
+        uint256 requestId = pickWinner();
+        emit RequestRaffleWinner(requestId);
+
+
+    }
 }
