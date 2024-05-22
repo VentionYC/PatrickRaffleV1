9a929144b2baa6b1ef8379a451cdaa95cf82fa3b
Vention <backstreetal6year@gmail.com>
2024-05-22 15:43:01 +0800

    roughly finished deploy based on network

--- script/Deploy.s.sol
+++ script/Deploy.s.sol
@@ -3,6 +3,8 @@
 pragma solidity ^0.8.18; 
 import {Script} from "forge-std/Script.sol";
 import {Raffle} from "../src/Raffle.sol";
+import {HelperConfig} from "./HelperConfig.s.sol";
+
 contract DeployRaffle is Script {
     //we need to create a run function
     //the run() funciton like the setup() function in the test, both the name should not be changed
@@ -17,6 +19,27 @@ contract DeployRaffle is Script {
         // some of the raffle input parameter above in the constructor, will depened on what chain we are using, 
         // so we can create Helperconfig to help with no matter what chain we are deploy to, we are good to go
     function run() external returns (Raffle) {
+        //Based on the active Network, can have diff config
+        HelperConfig helperConfig = new HelperConfig();
+        (
+                uint256 enterRaffleFee,
+                uint256 interval,
+                bytes32 gasLane,
+                address vrfCoordinator,
+                uint64 subscribtionId,
+                uint32 gasLimit
+        )= helperConfig.actviceNetWorkConfig();
 
+        vm.startBroadcast();
+        Raffle raffle = new Raffle(
+            enterRaffleFee,
+            interval,
+            gasLane,
+            vrfCoordinator,
+            subscribtionId,
+            gasLimit
+        );
+        vm.stopBroadcast();
+        return raffle;
     }
 }
--- script/HelperConfig.s.sol
+++ script/HelperConfig.s.sol
@@ -2,11 +2,11 @@
 pragma solidity ^0.8.18;
 
 import {Script} from "forge-std/Script.sol";
+import {VRFCoordinatorV2Mock} from "@chainlink/contracts/v0.8/mocks/VRFCoordinatorV2Mock.sol";
 
 contract HelperConfig is Script {
+    uint256 private constant SEPOLIA_CHAIN_ID = 11155111;
 
-
-    //We will need vrfCoordinator right?
     struct NetworkConfig {
                 uint256 enterRaffleFee;
                 uint256 interval;
@@ -14,5 +14,81 @@ contract HelperConfig is Script {
                 address vrfCoordinator;
                 uint64 subscribtionId;
                 uint32 gasLimit;
+    }
+
+    //set some active config
+    NetworkConfig public actviceNetWorkConfig;
+
+
+    //for the mock use?
+    //in order to test on the local test net,
+    // we at least have to have the gasLane and the vrfcooordinator
+    // since these two parameter we get from the other contract, 
+    //so we have to create the mocked local contract to get these value.
+
+    //This mocked contract already been done
+
+    constructor (){
+        if (block.chainid == SEPOLIA_CHAIN_ID) {
+            actviceNetWorkConfig = getSepoliaEthConfig();
+            
+        }else{
+            actviceNetWorkConfig = getOrCreateAnvilEthConfig();
+        }
+    }
+
+    
+    /**
+     * HARDCODED FOR SEPOLIA
+     * COORDINATOR: 0x8103B0A8A00be2DDC778e6e7eaa21791Cd364625
+     * ChainLink_COORDINATOR_URL: https://docs.chain.link/vrf/v2/subscription/supported-networks
+     */
+    //This is the test net config 
+    function getSepoliaEthConfig() public pure returns (NetworkConfig memory) {
+        return NetworkConfig({
+            enterRaffleFee: 0.01 ether,
+            interval: 30, //sec
+            gasLane: 0x474e34a077df58807dbe9c96d3c009b23b3c6d0cce433e59bbf5b34f823bc56c,
+            vrfCoordinator: 0x8103B0A8A00be2DDC778e6e7eaa21791Cd364625,
+            subscribtionId: 0, //Needed for update with the real subId!
+            gasLimit: 500000
+
+        });
+    }
+
+    //Local net we are using anvil network
+    //Test locally need using a mock contract 
+    //https://docs.chain.link/vrf/v2/subscription/examples/test-locally
+    //function getAnvilEthConfig() public view returns (NetworkConfig memory) {}
+
+    //in order to using the local test net, we have to deploy the mock contract first
+    function getOrCreateAnvilEthConfig() public returns(NetworkConfig memory) {
+        if (actviceNetWorkConfig.vrfCoordinator != address(0)) {
+            return actviceNetWorkConfig;
+        }
+
+        //constructor of the VRFCoordinatorV2Mock()
+        //1. baseFee
+        //2. gasPriceLink
+
+        uint96 baseFee = 0.25 ether; //0.25 Link
+        uint96 gasPriceLink = 1e9; //1 gwei LINK
+        //Deploy mocked contract
+        vm.startBroadcast();
+        VRFCoordinatorV2Mock vrfCoordinatorV2Mock = new VRFCoordinatorV2Mock(
+            baseFee, 
+            gasPriceLink
+            );
+        vm.stopBroadcast();
+
+        return NetworkConfig({
+            enterRaffleFee: 0.01 ether,
+            interval: 30, //sec
+            gasLane: 0x474e34a077df58807dbe9c96d3c009b23b3c6d0cce433e59bbf5b34f823bc56c,//doesn't matter, leave it like this
+            vrfCoordinator: address(vrfCoordinatorV2Mock),
+            subscribtionId: 0, //our script will add this????
+            gasLimit: 500000
+
+        });
     }
 }
